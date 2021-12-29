63e6563312a5ffefed6714af4628fa117cc02091
KScriven <kerryn.scriven@bbc.co.uk>
2021-12-29 14:53:41 +0000

    Update to css and anchor link

--- src/Contact.svelte
+++ src/Contact.svelte
@@ -7,3 +7,4 @@
 </header>
 <p>Fact: I have non-existent facebook, twitter, instagram, snapchat, [fill in more here ... ] accounts</p>
 <p>You are welcome to email me at {email} or find me on <a href={linkedIn}>LinkedIn</a></p>
+<p>Learning to code is hard work so you can keep me motivated by <a href="https://www.buymeacoffee.com/kerryn">buying me a coffee \~/</a></p>
