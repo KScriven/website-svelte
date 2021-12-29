caf52d7cbdf4fb857093e22e55f933422a885100
KScriven <kerryn.scriven@bbc.co.uk>
2021-12-29 14:49:04 +0000

    Update to anchor tag

--- src/Contact.svelte
+++ src/Contact.svelte
@@ -6,4 +6,4 @@
   <h2>How to shoot the breeze with me</h2>
 </header>
 <p>Fact: I have non-existent facebook, twitter, instagram, snapchat, [fill in more here ... ] accounts</p>
-<p>You are welcome to email me at {email} or find me on LinkedIn:{linkedIn}</p>
+<p>You are welcome to email me at {email} or find me on <a href={linkedIn}>LinkedIn</a></p>
